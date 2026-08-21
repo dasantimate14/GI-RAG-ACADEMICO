200f597a20747e109e402db887a9f86b5f8a5012
Diego Santimateo <diego.santimateo@utp.ac.pa>
2026-08-21 16:58:03 -0500

    Cambios de modelo de embedding con más dimensiones para mejorar la precisión del rag

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ CHUNK_OVERLAP = 300
 # ============================================
 # EMBEDDINGS
 # ============================================
-EMBEDDING_MODEL = "paraphrase-multilingual-MiniLM-L12-v2"
+EMBEDDING_MODEL = "multi-qa-mpnet-base-v2"
 
 # ============================================
 # BÚSQUEDA / RETRIEVAL
